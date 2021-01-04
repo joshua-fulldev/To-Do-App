7530b014a3b1f79b81bba778a94246d507415fc6
Joshua-fosu <68957490+Joshua-fosu@users.noreply.github.com>
2021-01-04 13:47:57 +0000

    First Commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser());
+app.use(express.static(__dirname));
+app.set('view engine', 'ejs');
+
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
+app.get("/", (req, res) => {
+    var d = new Date();
+    let options = {weekday: 'long', month: 'long', day: 'numeric'};
+    var realDay = d.toLocaleDateString("en-US", options);
+
+    res.render("list", {kindOfDay : realDay, newListItem : items})
+
+})
+
+app.post("/", (req, res) => {
+    const requestBody = req.body;
+    console.log(requestBody.next_item);
+    var item = requestBody.next_item;
+    items.push(item);
+
+    res.redirect("/");
+})
+
+app.listen(3000, () => {
+    console.log("server is working");
+})
